c82ffcc0d9b3bd7dda3b98603e989b646e6d58c6
LeonardoMartinsJava <166058790+LeonardoMartinsJava@users.noreply.github.com>
2024-04-04 15:39:00 -0300

    Add files via upload
    Exercicio resolvido do BeeCrowd ! Primeiro repositorio da conta.

---
+++ Main.java
@@ -0,0 +1,26 @@
+package cu;
+import java.util.Scanner;
+
+public class Main {
+	public static void main (String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int curupira = 300;
+		int boitata = 1500;
+		int boto = 600;
+		int mapinguari = 1000;
+		int iara = 150;
+		int donachica = 225;
+		
+		int cu2 = scan.nextInt();
+		int boi2 = scan.nextInt();
+		int boto2 = scan.nextInt();
+		int map2 = scan.nextInt();
+		int iara2 = scan.nextInt();
+		
+		int resultado = cu2 * curupira + boi2 * boitata + boto2 * boto + map2 * mapinguari
+				+ iara2 * iara + donachica;
+		
+		System.out.println(resultado +  "\n");
+		
+	}
+}
